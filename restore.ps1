517d1f7a6dd01cc598f74d36f552ed6577d3f68c
H4V1XX <dcordovez363@gmail.com>
2026-09-05 15:47:38 +0100

    Delete restore.ps1

--- restore.ps1
+++
@@ -1,2 +0,0 @@
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 1 /f
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 5 /f
